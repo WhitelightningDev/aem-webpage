973f618bab0ce0ca3b3861b4824eed9d2f704419
Daniel <danielmommsen2@gmail.com>
2025-03-26 11:08:46 +0200

    final update of the website

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';  // Import MatDialog
 import { NewMembershipDialogComponent } from '../new-membership-dialog/new-membership-dialog.component';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-footer',
+  imports: [MatIconModule],
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.css']
 })
